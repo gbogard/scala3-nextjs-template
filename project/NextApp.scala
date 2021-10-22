f4c850049da586ad6a5e565c30065f566a5810c0
Guillaume Bogard <hey@guillaumebogard.dev>
2021-10-22 13:35:57 +0200

    Release v1.1.0
    - Updated scalajs-react to the latest v2 RC (2.0.0-RC4)
    - Added support for scalajs-react's core-generic module (See [Effect
      agnosticism](https://github.com/japgolly/scalajs-react/blob/master/doc/FX_AGNOSTICISM.md))
    - Fixed the default npm path (previously was my personal config, whoops)

--- project/NextApp.scala
+++ project/NextApp.scala
@@ -13,10 +13,18 @@ import scala.sys.process._
 object NextApp extends AutoPlugin {
   override def requires = ScalaJSPlugin
   override def trigger = noTrigger
-  private val scalaJsReactVersion = "2.0.0-RC3"
 
   object autoImport {
     val npm = SettingKey[String]("npm", "The path to the npm executable")
+    val scalaJsReactVersion =
+      SettingKey[String](
+        "scalaJsReactVersion",
+        "The version of scalaJsReact to use"
+      )
+    val scalaJsReactUseGeneric = SettingKey[Boolean](
+      "scalaJsReactUseGeneric",
+      "Whether to use the 'core-generic' module scalajs-react, that provides effect agnositicism"
+    )
     val nextServerProcess = AttributeKey[Option[Process]](
       "nextServerProcess",
       "A handle to the currently-running next dev server"
@@ -51,10 +59,20 @@ object NextApp extends AutoPlugin {
     (Compile / fastLinkJS / scalaJSLinkerConfig) ~= {
       _.withModuleSplitStyle(ModuleSplitStyle.SmallestModules)
     },
-    libraryDependencies ++= Seq(
-      "com.github.japgolly.scalajs-react" %%% "core" % scalaJsReactVersion
-    ),
-    npm := "/Users/gbogard/.nix-profile/bin/npm",
+    scalaJsReactVersion := "2.0.0-RC4",
+    scalaJsReactUseGeneric := false,
+    libraryDependencies ++=
+      (if (scalaJsReactUseGeneric.value) {
+         Seq(
+           "com.github.japgolly.scalajs-react" %%% "core-generic" % scalaJsReactVersion.value,
+           "com.github.japgolly.scalajs-react" %%% "util-dummy-defaults" % scalaJsReactVersion.value
+         )
+       } else {
+         Seq(
+           "com.github.japgolly.scalajs-react" %%% "core" % scalaJsReactVersion.value
+         )
+       }),
+    npm := "npm",
     startNextServer := {
       if (!nextServerIsRunning.value) {
         val log = streams.value.log
@@ -84,6 +102,6 @@ object NextApp extends AutoPlugin {
     },
     npmInstall := Process(Seq(npm.value, "install"), baseDirectory.value).!,
     nextBuild := Process(Seq(npm.value, "run", "build"), baseDirectory.value).!,
-    nextBuild := nextBuild.dependsOn(Compile / fullLinkJS).value,
+    nextBuild := nextBuild.dependsOn(Compile / fullLinkJS).value
   )
 }
